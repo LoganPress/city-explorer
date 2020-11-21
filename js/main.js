3b2a939c21637b3615861a2a07793bdc553ea560
Logan Press <loganpress@Logans-MacBook-Pro.local>
2020-11-21 15:40:30 -0600

    Switching to leaflet for now

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-d3.json("/data/Neighborhood_Boundaries.json").then(function(data) {
-    let neighborhoodVis = new NeighborhoodMap("neighborhood-vis", data)
-})
+let neighborhoodVis;
+
+$.getJSON("/data/Neighborhood_Boundaries.json", function(geoFeatures) {
+    const mapPosition = [38.6270, -90.1994];
+    neighborhoodVis = new NeighborhoodMap("neighborhood-vis", [], geoFeatures.features, mapPosition);
+});
--- js/neighborhoodMap.js
+++ js/neighborhoodMap.js
@@ -2,11 +2,12 @@
     The visualization for the neighborhoods
 */
 
-NeighborhoodMap = function(_parentElement, _data, _geoFeatures) {
+NeighborhoodMap = function(_parentElement, _data, _geoFeatures, _mapPosition) {
 
 	this.parentElement = _parentElement;
 	this.data = _data;
-	this.geoFeatures = _geoFeatures;
+    this.geoFeatures = _geoFeatures;
+    this.mapPosition = _mapPosition;
 
 	this.initVis();
 }
@@ -14,14 +15,30 @@ NeighborhoodMap = function(_parentElement, _data, _geoFeatures) {
 
 NeighborhoodMap.prototype.initVis = function() {
     let vis = this;
-    vis.margin = { left: 0, top: 0, right: 0, bottom: 0 }
-    vis.width = 700 - vis.margin.left - vis.margin.right;
-    vis.height = 500 - vis.margin.top - vis.margin.bottom;
-    vis.svg = d3.select("#" + vis.parentElement)
-        .append("svg")
-        .attr("width", vis.width)
-        .attr("height", vis.height)
-        .attr("id", "neighborhood-map");
+    vis.map = L.map(vis.parentElement).setView(vis.mapPosition, 13);
+    L.geoJson(vis.geoFeatures, {
+        style: {
+            fill: "ADD8E6",
+            color: "blue"
+        },
+        weight: 5,
+        opacity: 0.7
+    }).addTo(vis.map);
+    L.Icon.Default.imagePath = '/img/';
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+   		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(vis.map);
+
+    // vis.margin = { left: 0, top: 0, right: 0, bottom: 0 }
+    // vis.width = 700 - vis.margin.left - vis.margin.right;
+    // vis.height = 500 - vis.margin.top - vis.margin.bottom;
+    // vis.svg = d3.select("#" + vis.parentElement)
+    //     .append("svg")
+    //     .attr("width", vis.width)
+    //     .attr("height", vis.height)
+    //     .attr("id", "neighborhood-map");
+
     vis.wrangleData();
 };
 
@@ -34,20 +51,20 @@ NeighborhoodMap.prototype.wrangleData = function() {
 NeighborhoodMap.prototype.updateVis = function() {
     let vis = this;
 
-    let projection = d3.geoMercator()
-        .translate([vis.width / 2, vis.height / 2])
-        .scale(200);
-    let path = d3.geoPath().projection(projection);
-    let city = vis.data.features;
+    // let projection = d3.geoMercator()
+    //     .translate([vis.width / 2, vis.height / 2])
+    //     .scale(200);
+    // let path = d3.geoPath().projection(projection);
+    // let city = vis.data.features;
 
-    let map = vis.svg.selectAll("path")
-        .data(city)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .attr("fill", "white")
-        .attr("stroke-width", 1)
-        .attr("stroke", "black");
+    // let map = vis.svg.selectAll("path")
+    //     .data(city)
+    //     .enter()
+    //     .append("path")
+    //     .attr("d", path)
+    //     .attr("fill", "white")
+    //     .attr("stroke-width", 1)
+    //     .attr("stroke", "black");
 
-    map.exit().remove();
+    // map.exit().remove();
 };
