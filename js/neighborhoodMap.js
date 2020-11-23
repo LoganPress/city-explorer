7dc154a82e491be9e172cbe4074185edbc94aa22
Logan P <lpress@wustl.edu>
2020-11-22 21:03:58 -0600

    Made the map work again for now

--- js/neighborhoodMap.js
+++ js/neighborhoodMap.js
@@ -16,7 +16,7 @@ NeighborhoodMap = function(_parentElement, _data, _geoFeatures, _mapPosition, _g
 
 NeighborhoodMap.prototype.initVis = function() {
     let vis = this;
-    console.log(vis.data.population['1']);
+    // console.log(vis.data.population['1']);
     vis.map = L.map(vis.parentElement).setView(vis.mapPosition, 12);
     let neighborhoodLayer = L.geoJson(vis.geoFeatures, {
         color: "#3F7484",
@@ -27,8 +27,8 @@ NeighborhoodMap.prototype.initVis = function() {
     function onEachNeighborhood(n, layer) {
         const parkIds = [80, 81, 82, 83, 84, 85, 86, 87];
         const num = toString(n.properties.NHD_NUM);
-        const population = vis.data.population[num];
-        layer.bindTooltip('<h2>' + n.properties.NHD_NAME + '</h2><strong>' + population + '</strong>', {
+        // const population = vis.data.population[num];
+        layer.bindTooltip('<h2>' + n.properties.NHD_NAME + '</h2><strong>Hello</strong>', {
             sticky: true
         });
         if (!parkIds.includes(num)) {
