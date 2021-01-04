27b801f6a439aff4678a7d86a8e566da4e722438
Logan P <lpress@wustl.edu>
2021-01-03 19:34:00 -0800

    Made selected neighborhood list work a little better

--- js/neighborhoodList.js
+++ js/neighborhoodList.js
@@ -34,28 +34,49 @@ NeighborhoodList.prototype.updateVis = function(neighborhood) {
     let vis = this;
 
     if (neighborhood.nid != 0) {
-        if (vis.data.includes(neighborhood)) {
-            let index = vis.data.indexOf(neighborhood);
+        if (vis.data.map((d) => d.nid).includes(neighborhood.nid)) {
+            let index = vis.data.indexOf(vis.data.find((d) => d.nid == neighborhood.nid));
             vis.data.splice(index, 1);
         } else {
             vis.data.push(neighborhood);
         }
     }
+    
+    console.log(vis.data);
 
-    vis.rects = vis.svg.selectAll("rect")
+    vis.rects = vis.svg.selectAll(".selected-box")
         .data(vis.data, (d) => d.nid)
         .enter()
         .append("rect")
-        .attr("height", 20)
-        .attr("width", 20)
-        .attr("x", 5)
-        .attr("y", (d, i) => 25 * i + 2)
-        .attr("class", "selected-box")
-        .attr("id", (d) => "selected-box-" + d.nid)
-        .attr("fill", (d) => "rgb(" + d.color.join(",") + ")")
-        .on("click", function(e, d) {
-            console.log("Clicked");
-        });
+            .attr("height", 20)
+            .attr("width", 20)
+            .attr("x", vis.svg.attr("width") / 2 - 100)
+            .attr("y", (d, i) => 30 * i + 5)
+            .attr("class", "selected-box")
+            .attr("id", (d) => "selected-box-" + d.nid)
+            .attr("fill", (d) => "rgb(" + d.color.join(",") + ")")
+            .on("click", function(e, d) {
+                cityVis.selectedNeighborhoods[d.nid-1] = !cityVis.selectedNeighborhoods[d.nid-1];
+                cityVis.updateVis();
+                cityVis.parallelPlot.updateVis(cityVis.data[d.nid-1]);
+                vis.updateVis(d);
+            });
 
     vis.rects.exit().remove();
+
+    vis.names = vis.svg.selectAll(".selected-name")
+        .data(vis.data, (d) => d.nid)
+        .enter()
+        .append("text")
+            .text((d) => d.name)
+            .attr("x", vis.svg.attr("width") / 2 - 50)
+            .attr("y", (d, i) => 30 * i + 20)
+            .attr("class", "selected-name")
+            .style("text-anchor", "start")
+            .style("font-weight", "bold")
+            .style("font-size", 18)
+            .attr("fill", "black");
+
+    vis.names.exit().remove();
+
 }
